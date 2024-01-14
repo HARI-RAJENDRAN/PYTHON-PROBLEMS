6f2ad1079838632adeadbb4dd2f45f465d5df90f
HARI KRISHNAN <141993320+HARI-CHARLEZ@users.noreply.github.com>
2024-01-14 20:42:38 +0530

    Update user input program.py

--- user input program.py
+++ user input program.py
@@ -1,8 +1,8 @@
 #python basic(user input method)
 
-value_a=int(input("enter the value to add: "))
-value_b=int(input("enter the value to add: "))
-value_c=input("enter any operator ( + , - , * , - ): ")
+value_a=int(input("enter the first value to calculate: "))
+value_b=int(input("enter the second value to calculate: "))
+value_c=input("enter any operator ( + , - , * , / ): ")
 if(value_c == "+"):
   print(value_a + value_b)
 elif(value_c=="-"):
@@ -12,4 +12,4 @@ elif(value_c=="/"):
 elif(value_c=="*"):
   print(value_a * value_b)
 else:
-  print("go learn maths")
+  print("go learn maths first")
